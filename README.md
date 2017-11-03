1553816fb19345e23e648d198e62b65a89687575
Eric Whyne <ericwhyne@datamachines.io>
2017-11-02 22:06:47 -0400

    first

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # trello-activity-bot
+
+Posts in a slack channel if Trello boards haven't been updated. This prevents
+dead or abandoned boards from going unnoticed and ensures adherence to company
+policy.
+
+## Installation and Use
+1. edit trellobot.config to contain your keys and preferences.  
+2. Place trellobot.config in the directory ~/.trellobot/  
+3. Run trellobot.py in a crontab  
+
+crontab -e  
+0 7 * * * /Users/eric/workspace/trello-activity-bot/trellobot.py   
---
+++ trellobot.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import requests
+from os.path import expanduser
+import yaml
+from trello import TrelloApi
+import json
+from datetime import datetime, timedelta
+
+inactivityThreshold = 3 # in number of days
+
+config_file = expanduser("~") + '/.trellobot/trellobot.config'
+config = yaml.safe_load(open(config_file))
+
+runDate = datetime.now().strftime("%Y-%m-%d")
+runDateObj = datetime.now()
+
+trello = TrelloApi(config['api-key'])
+trello.set_token(config['api-token'])
+
+boards = trello.organizations.get_board(config['org_name'])
+message = []
+for board in boards:
+    bName = board['name']
+    lastAct = board['dateLastActivity']
+    if lastAct is not None:
+        lastActObj = datetime.strptime(lastAct, "%Y-%m-%dT%H:%M:%S.%fZ")
+        test = lastActObj.strftime("%Y-%m-%d")
+        if runDateObj - timedelta(days = inactivityThreshold) > lastActObj:
+            line = "Trello board " + bName + " hasn't had activity in " + \
+                str(inactivityThreshold) + " days. :slightly_frowning_face:"
+            message.append(line)
+text = "\n".join(message)
+print text
+slack_payload = {
+    "text": text,
+    "channel": config['channel'],
+    "icon_emoji": config['emoji'],
+    "username": config['username']
+}
+# TODO: add error handling here
+r = requests.post(config['slack_webhook_url'], data = json.dumps(slack_payload))
+print slack_payload
